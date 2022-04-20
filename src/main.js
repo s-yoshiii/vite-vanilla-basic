033a830fe481b80144aff5d91d15a3f97f74289f
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-04-20 23:46:47 +0900

    vite開発環境の構築

--- src/main.js
+++ src/main.js
@@ -1,6 +1,1 @@
-import "./style.css";
-
-document.querySelector("#app").innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="/detail.html" target="_blank">Documentation</a>
-`;
+console.log("main.js");
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,28 @@
 import { defineConfig } from "vite";
+import legacy from "@vitejs/plugin-legacy";
 import path from "path";
 export default defineConfig({
   root: "src",
+  base: "/",
   build: {
-    outDir: "/dist",
+    outDir: "../dist",
   },
-
+  sourcemap: process.env.NODE_ENV !== "production",
   server: {
     port: 8000,
     open: true,
+  },
+  plugins: [
+    legacy({
+      targets: ["ie >= 11"],
+      additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `$injectedColor: orange;`,
+      },
+    },
   },
 });
