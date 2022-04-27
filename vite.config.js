49c9118e587b13ef5f1b702d3ae34fd15aa848e9
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-04-28 00:35:10 +0900

    vite設定変更、マルチページの場合想定

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite";
 import legacy from "@vitejs/plugin-legacy";
-import path from "path";
+const { resolve } = require("path");
+const root = "src";
 export default defineConfig({
-  root: "src",
+  root: root,
   base: "/",
   build: {
     outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, root, "index.html"),
+        page: resolve(__dirname, root, "page/index.html"),
+      },
+    },
   },
   sourcemap: process.env.NODE_ENV !== "production",
   server: {
