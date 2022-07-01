fb544fe6c7c1d2346a0f39f3e1d7c87bc1d079b0
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2022-07-02 00:58:41 +0900

    環境構築

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,37 @@
 import { defineConfig } from "vite";
 import legacy from "@vitejs/plugin-legacy";
 const { resolve } = require("path");
+import viteImagemin from "vite-plugin-imagemin";
 const root = "src";
 export default defineConfig({
   root: root,
   base: "/",
   build: {
     outDir: "../dist",
+    emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, root, "index.html"),
+        main: resolve(__dirname, root, "assets/js/main.js"),
+        index: resolve(__dirname, root, "index.html"),
         page: resolve(__dirname, root, "page/index.html"),
+      },
+      output: {
+        assetFileNames: (assetInfo) => {
+          console.log(assetInfo);
+          let extType = assetInfo.name.split(".")[1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = "images";
+          } else if (/woff|woff2|s?css/.test(extType)) {
+            extType = "css";
+          }
+          return `assets/${extType}/[name][extname]`;
+        },
+        chunkFileNames: "assets/js/[name].js",
+        entryFileNames: "assets/js/[name].js",
       },
     },
   },
-  sourcemap: process.env.NODE_ENV !== "production",
+  // sourcemap: process.env.NODE_ENV !== "production",
   server: {
     port: 8000,
     open: true,
@@ -24,12 +41,39 @@ export default defineConfig({
       targets: ["ie >= 11"],
       additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
     }),
-  ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `$injectedColor: orange;`,
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
       },
-    },
-  },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      pngquant: {
+        quality: [0.8, 0.9],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: "removeViewBox",
+          },
+          {
+            name: "removeEmptyAttrs",
+            active: false,
+          },
+        ],
+      },
+    }),
+  ],
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       additionalData: `$injectedColor: orange;`,
+  //     },
+  //   },
+  // },
 });
